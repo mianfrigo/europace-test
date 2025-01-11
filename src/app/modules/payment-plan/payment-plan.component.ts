e1d91ab87b3e935687ec3a5507b347a4d423f4a4
Miguel Frias <mianfrigo@gmail.com>
2025-01-11 14:54:39 +0100

    feat(eruopace/app): Create modules, components, services & models

---
+++ src/app/modules/payment-plan/payment-plan.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-payment-plan',
+  imports: [],
+  templateUrl: './payment-plan.component.html',
+  styleUrl: './payment-plan.component.scss'
+})
+export class PaymentPlanComponent {
+
+}
